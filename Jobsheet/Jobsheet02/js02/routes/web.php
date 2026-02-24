4dcbc8e4fa463e01eea3ae6cdaea523af5931e29
fitraadhim <azzazria123@gmail.com>
2026-02-24 22:25:29 +0700

    praktikum 1 selesai

--- Jobsheet/Jobsheet02/js02/routes/web.php
+++ Jobsheet/Jobsheet02/js02/routes/web.php
@@ -6,30 +6,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello World!';
-});
+// Route::get('/hello', function () {
+//     return 'Hello World!';
+// });
 
-Route::get('/world', function () {
-    return "World!";
-});
+// Route::get('/world', function () {
+//     return "World!";
+// });
 
-Route::get('/selamat', function () {
-    return "Selamat Datang";
-});
+// Route::get('/selamat', function () {
+//     return "Selamat Datang";
+// });
 
-Route::get('/about', function () {
-    return "Nama Saya Adalah Muhammad Fitra Adhim Nurrochman, NIM saya 244107020089";
-});
+// Route::get('/about', function () {
+//     return "Nama Saya Adalah Muhammad Fitra Adhim Nurrochman, NIM saya 244107020089";
+// });
 
-Route::get('/user/{name}', function ($name) {
-    return ('Nama saya ' .$name);
-});
+// Route::get('/user/{name}', function ($name) {
+//     return ('Nama saya ' .$name);
+// });
 
-Route::get('/posts/{post}/{comments}/{comment}', function ($postId, $commentId) {
-    return ('Pos ke-' .$postId . "Komentar ke- ".$commentId);
-});
+// Route::get('/posts/{post}/{comments}/{comment}', function ($postId, $commentId) {
+//     return ('Pos ke-' .$postId . "Komentar ke- ".$commentId);
+// });
 
-Route::get('/articles/{id}', function ($id) {
-    return ('Halaman dengan ID Artikel: ' .$id);
+// Route::get('/articles/{id}', function ($id) {
+//     return ('Halaman dengan ID Artikel: ' .$id);
+// });
+
+// Route::get('/user/{name?}', function ($name = null) {
+//     return ('Nama saya: ' .$name);
+// });
+
+Route::get('/user/{name?}', function ($name = "John") {
+    return ('Nama saya: ' .$name);
 });
