01f9792d5d6a6d0add0c2696b193f6f594433794
fitraadhim <azzazria123@gmail.com>
2026-02-24 22:09:46 +0700

    Jobsheet02 route

---
+++ Jobsheet/Jobsheet02/js02/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello World!';
+});
+
+Route::get('/world', function () {
+    return "World!";
+});
+
+Route::get('/selamat', function () {
+    return "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    return "Nama Saya Adalah Muhammad Fitra Adhim Nurrochman, NIM saya 244107020089";
+});
+
+Route::get('/user/{name}', function ($name) {
+    return ('Nama saya ' .$name);
+});
+
