c922f98684b17a42ff8f4cdfdc4724138af58ef8
fitraadhim <azzazria123@gmail.com>
2026-02-24 22:16:31 +0700

    route articles/id

--- Jobsheet/Jobsheet02/js02/routes/web.php
+++ Jobsheet/Jobsheet02/js02/routes/web.php
@@ -26,3 +26,10 @@ Route::get('/user/{name}', function ($name) {
     return ('Nama saya ' .$name);
 });
 
+Route::get('/posts/{post}/{comments}/{comment}', function ($postId, $commentId) {
+    return ('Pos ke-' .$postId . "Komentar ke- ".$commentId);
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return ('Halaman dengan ID Artikel: ' .$id);
+});
